5912a9882ca8ea8c9ae13ec236c21f1132fb75f3
Olga <barsolet@mail.ru>
2018-09-25 22:47:49 +0300

    solution task5

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var obj = {};
+
+    if (currency > 10000) {
+        obj.error = "You are rich, my friend! We don't have so much coins for exchange";
+        return obj;
+    }
+
+    if (currency <= 0) {
+        return obj;
+    }
+
+    if (currency >= 50) {
+        var h = parseInt(currency/50);
+        obj.H = h;
+        currency = currency - ( h * 50 );
+    }
+
+    if (currency >= 25) {
+        var q = parseInt(currency/25);
+        obj.Q = q;
+        currency = currency - ( q * 25 );
+    }
+
+    if (currency >= 10) {
+        var d = parseInt(currency/10);
+        obj.D = d;
+        currency = currency - ( d * 10 );
+    }
+
+    if (currency >= 5) {
+        var n = parseInt(currency/5);
+        obj.N = n;
+        currency = currency - ( n * 5 );
+    }
+
+    if (currency < 5 && currency > 0) {
+        var p = currency;
+        obj.P = p;
+    }
+
+    return obj;
 }
